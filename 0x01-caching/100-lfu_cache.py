c3c875a57a71726dab527b28ad5e7645ab91ea91
Gift Mbonani <giftmbonani@outlook.com>
2024-10-31 21:26:49 +0000

    caching

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,40 +1,56 @@
-#!/usr/bin/python3
-"""LFU caching"""
-BaseCaching = __import__("base_caching").BaseCaching
+#!/usr/bin/env python3
+""" BaseCaching """
+from base_caching import BaseCaching
 
 
 class LFUCache(BaseCaching):
-    """LFU Cache"""
+    """LFU caching """
 
     def __init__(self):
-        """initialize with parent's init method"""
+        """Initialize"""
         super().__init__()
-        self.freq = {}
+        self.history = []
+        self.frequency = {}
 
     def put(self, key, item):
-        """assign a new value key pair"""
+        """assign a new key-pair value """
         if key is None or item is None:
-            return
-
-        if key in self.cache_data:
-            self.cache_data[key] = item
-            self.freq[key] += 1
+            pass
         else:
-            if len(self.cache_data) >= self.MAX_ITEMS:
-                min_freq = min(self.freq.values())
-                least_freq_keys = [
-                    k for k, v in self.freq.items() if v == min_freq
-                ]
-                lfu_key = min(least_freq_keys, key=self.freq.get)
-                self.cache_data.pop(lfu_key)
-                self.freq.pop(lfu_key)
-                print("DISCARD:", lfu_key)
+            length = len(self.cache_data)
+            if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+                lfu = min(self.frequency.values())
+                lfu_keys = []
+                for k, v in self.frequency.items():
+                    if v == lfu:
+                        lfu_keys.append(k)
+                if len(lfu_keys) > 1:
+                    lru_lfu = {}
+                    for k in lfu_keys:
+                        lru_lfu[k] = self.history.index(k)
+                    discard = min(lru_lfu.values())
+                    discard = self.history[discard]
+                else:
+                    discard = lfu_keys[0]
 
+                print("DISCARD: {}".format(discard))
+                del self.cache_data[discard]
+                del self.history[self.history.index(discard)]
+                del self.frequency[discard]
+            if key in self.frequency:
+                self.frequency[key] += 1
+            else:
+                self.frequency[key] = 1
+            if key in self.history:
+                del self.history[self.history.index(key)]
+            self.history.append(key)
             self.cache_data[key] = item
-            self.freq[key] = 1
 
     def get(self, key):
-        """return value"""
-        if key in self.cache_data:
-            self.freq[key] += 1
-            return self.cache_data.get(key)
+        """ returns the value"""
+        if key is not None and key in self.cache_data.keys():
+            del self.history[self.history.index(key)]
+            self.history.append(key)
+            self.frequency[key] += 1
+            return self.cache_data[key]
+        return None
--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,28 +1,34 @@
-#!/usr/bin/python3
-""" LRU caching """
-BaseCaching = __import__("base_caching").BaseCaching
+#!/usr/bin/env python3
+""" LRU caching"""
+
+
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """LRU Caching"""
-
+    """ LRU caching"""
     def __init__(self):
         super().__init__()
-        self.order = []
+        self.history = []
 
     def put(self, key, item):
-        """assign new item to dictionary"""
-        if key and item:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                removed = self.order.pop(0)
-                self.cache_data.pop(removed)
-                print("DISCARD: {}".format(removed))
+        """ assigns new item to the dictionary"""
+        if not (key is None or item is None):
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS\
+                 and key not in self.cache_data:
+                print(f'DISCARD: {self.history[0]}')
+                self.cache_data.pop(self.history[0])
+                del self.history[0]
+            if key in self.history:
+                del self.history[self.history.index(key)]
+            self.history.append(key)
             self.cache_data[key] = item
-            self.order.append(key)
 
     def get(self, key):
-        """return value"""
-        if key in self.cache_data:
-            self.order.remove(key)
-            self.order.append(key)
-            return
+        """ returns the value"""
+        if key is None or not (key in self.cache_data):
+            return None
+        else:
+            del self.history[self.history.index(key)]
+            self.history.append(key)
+            return self.cache_data[key]
